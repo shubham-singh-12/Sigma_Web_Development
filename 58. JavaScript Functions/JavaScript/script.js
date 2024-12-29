013fc1c88e2963ecf70288d1cd55f68b62a27245
shubham-singh-12 <shubhamytsingh@gmail.com>
2024-12-29 19:48:46 +0530

    Functions in JS

---
+++ 58. JavaScript Functions/JavaScript/script.js
@@ -0,0 +1,53 @@
+// console.log('Checking Console...');
+
+
+
+// functions
+// function nice(name) {
+//   console.log("Hey, " + name + " you are nice.");
+//   console.log("Hey, " + name + " you are good.");
+//   console.log("Hey, " + name + " good morning.");
+// };
+
+// nice("Shubham");
+
+
+
+// function sum(a, b) {
+//   console.log(a + b);
+// }
+
+// sum(2, 3);  //output: 5
+
+
+
+// function sum(a, b, c = 3) {
+//   console.log(a + b + c);
+// }
+
+// sum(2, 3);    //output: 8
+
+
+
+// function sum(a, b, c = 3) {
+//   console.log(a + b + c);
+// }
+
+// sum(2, 3, 1);    //output: 6
+
+
+
+// Arrow Function
+// const func1 = () => {
+//   console.log("Hey, i am an Arrow Function.");
+// }
+
+// func1();  //output: Hey, i am an Arrow Function 
+
+
+
+const func1 = (x) => {
+  console.log("Hey, my value is:", x);
+}
+
+func1(12);  //output: Hey, my value is: 12
