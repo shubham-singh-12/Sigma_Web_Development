57251a0d99e3eb969aebe32454e8dfc9ec6c2c6c
shubham-singh-12 <shubhamytsingh@gmail.com>
2025-01-31 09:10:01 +0530

    Selectors in JS

---
+++ 68. JavaScript - Selecting ids, Classes, More/JavaScript/script.js
@@ -0,0 +1,42 @@
+// console.log("Hello, world");
+
+
+// getElementByClassName
+// let boxes = document.getElementsByClassName('box');
+// console.log(boxes);
+// boxes[2].style.backgroundColor = 'red';
+
+
+//=======================================================================================================================================================================
+
+
+// getElementById
+// let boxes = document.getElementById('redbox').style.backgroundColor = 'red';
+
+
+//=======================================================================================================================================================================
+
+
+// querySelector
+// document.querySelector('.box').style.backgroundColor = 'green';
+
+
+//=======================================================================================================================================================================
+
+
+// querySelectorAll
+
+// a. To change a particular element.
+// document.querySelectorAll('.box')[0].style.backgroundColor = 'green';
+
+// b. To change all elements
+// document.querySelectorAll('.box').forEach(e => {
+//   e.style.backgroundColor = 'green';
+// }); 
+
+
+//=======================================================================================================================================================================
+
+
+// getElementsByTagName
+document.getElementsByTagName('div')[1].style.backgroundColor = 'green';
