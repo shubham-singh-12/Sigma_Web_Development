830255749dc90d81e00e022f150329938d2675b5
shubham-singh-12 <shubhamytsingh@gmail.com>
2025-01-31 09:08:33 +0530

    DOM in JS

---
+++ 66. Document Object Model in JaaScript/JavaScript/script.js
@@ -0,0 +1,9 @@
+// Change title of Webpage
+document.title = 'Shubham is Good';
+
+
+// Show content of body in console
+console.log(document.body);
+
+// Change background color to green
+document.body.style.backgroundColor = 'Green';
