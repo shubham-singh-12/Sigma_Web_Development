ff911ccba84538f4660c6f55dc176607dd5f8178
shubham-singh-12 <shubhamytsingh@gmail.com>
2025-01-31 09:05:48 +0530

    Exercise 10 - Business Name Generator

---
+++ 62. JavaScript Exercise 10 - Business Name Generator/JavaScript/script.js
@@ -0,0 +1,30 @@
+// let adjective_1 = Crazy;
+// let adjective_2 = Amazing;
+// let adjective_3 = Fire;
+
+// let shopName_1 = Engine;
+// let shopName_2 = Foods;
+// let shopName_3 = Garments;
+
+// let anotherWord_1 = Bros;
+// let anotherWord_2 = Limited;
+// let anotherWord_3 = Hub;
+
+let adjective = {
+  adjective_1: 'Crazy',
+  adjective_2: 'Amazing',
+  adjective_3: 'Fire'
+};
+
+let shopName = {
+  shopName_1: 'Engine',
+  shopName_2: 'Foods',
+  shopName_3: 'Gaurments'
+};
+
+let anotherWord = {
+  anotherWord_1: 'Bros',
+  anotherWord_2: 'Limited',
+  anotherWord_3: 'Hub'
+};
+
